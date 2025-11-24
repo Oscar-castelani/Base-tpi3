321dd30bfa584bfb82867df876d7ee69849aef56
oscar-castelani <castelani83@gmail.com>
2025-11-24 17:33:56 -0300

    Separé el catálogo en secciones y agregué productos

---
+++ public/scripts/hogar.js
@@ -0,0 +1,48 @@
+// public/scripts/hogar.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  try {
+    fetch('http://localhost:3000/api/products/hogar')
+      .then(response => response.json())
+      .then(products => {
+        const productList = document.getElementById('product-list');
+
+        products.forEach(product => {
+          const listItem = document.createElement('div');
+          listItem.classList.add('col-lg-3', 'col-sm-6', 'my-3');
+
+          listItem.innerHTML = `
+            <div class="col-12 text-center h-100 product-item">
+              <div class="row h-100">
+                <div class="col-12 p-0 mb-3">
+                  <img src="${product.urlImg}" class="img-fluid" alt="${product.name}">
+                </div>
+
+                <div class="col-12 mb-3">
+                  <span class="product-name">${product.name}</span>
+                </div>
+
+                <div class="col-12 mb-2 product-description">
+                  ${product.description}
+                </div>
+
+                <div class="col-12 mb-3">
+                  <span class="product-price">
+                    $${product.price}
+                  </span>
+                </div>
+              </div>
+            </div>
+          `;
+
+          productList.appendChild(listItem);
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching hogar products:', error);
+      });
+  } catch (e) {
+    console.log('try-catch error en hogar.js');
+    console.log(e);
+  }
+});
